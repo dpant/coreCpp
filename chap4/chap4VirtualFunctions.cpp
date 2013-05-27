e3b7caa3a9b24ecdbf5e8ed1807076eca6228334
DeepakPant <dpant@gmail.com>
2013-05-27 15:57:42 -0700

    code snippet from Core C++ part 4 Stephan T. Lavavej

--- chap4/chap4VirtualFunctions.cpp
+++ chap4/chap4VirtualFunctions.cpp
@@ -15,33 +15,32 @@ http://channel9.msdn.com/Series/C9-Lectures-Stephan-T-Lavavej-Core-C-/Stephan-T-
 */
 #include <iostream>
 #include <ostream>
-#define TEST22 1 
+#define TEST26 1 
 
 
 /*
-==================== PART 3 ==================================================================================================
-======================================overload Resolution==================================================================
-1) add candidate function to the overload set , some of these candidate will come from template argument deduction
-    overload resolution does not look into the return type. it completely ignores it.
-2) "Viability Test:" Is candidate a viable fucntion ? , matches the number of arguments with the number of parameters. 
-    Also check if the argment provided is convertable (by implicity conversion)  to the parameter specified.
-	eg::
-	foo(int,int)   
-	foo(int,int,int);
-	foo(1,2); the first foo will be selected becuase it is the only vaiable candidate
+==================== PART 4 ==================================================================================================
+======================================Virtual Functions==================================================================
+* Some of the snippet does not work with gcc 4.6 need to use gcc 4.7 or higher
+   in ubuntu use "sudo apt-get install gcc-snapshot"
+   than you can use the experimental version of gcc 4.8 located at 	/usr/lib/gcc-snapshot/bin/gcc
 
-	int meow(int)
-	int meow(const string &)
+   * changed the makefile to take gcc from /usr/lib/gcc-snapshot/bin/gcc. (4.7 or higher) 
 
-	meow(11) //int // second candidate will fail the viabiity test since int is not convertable to string.
-	meow(s) // string --
-	meow("peperment"); // const char[N]
 
-	Both option "int meow(int)" and "int meow(const string &)" are vaiable candidate 
+	* inherentence is usually overused. compiler use vptr and vtable to implement the virtual function mechanism
+	* inhertence is not the only way you can achieve code reuse.!!
+	* Remember inheretence create tight coupling between base and derived class. Relationship between the base and the derived
+	  class is second closest which can happen in C++. the most closest is the friendship in which class knows everything about
+	  the other class
+    * Remember object oriented programming is "to use object to encapsulate state and behaviour"
 
-3)  Best viable function concept
-		some implicit conversion of argment are better than others  
+	* Every class have its static type and dynamic type.
 
+	* REMEMBER VIRTUAL FUNCTION MECHANISM IS BASED ON RUNTIME AND COMPLETELY INDEPENDENT WITH THE COMPILE TIME ACCESS SPECIFIER SO YOU CAN 
+      EVEN SPECIFY THE PRIVATE SPECIFIER IN THE DERIVED CALSS FOR THE VIRTUAL FUNTION AND STILL DERIVED FUNCTION WILL BE ACCESSIBLE. 
+      ACCESS CONTROL CHECK IS APPLIED STATICALLY 
+	
 
 */
 #include <vector> // puts vector in std namespace
@@ -50,140 +49,8 @@ http://channel9.msdn.com/Series/C9-Lectures-Stephan-T-Lavavej-Core-C-/Stephan-T-
 #include <iostream>
 using namespace std;
 
-
-#if TEST18
+#if TEST23
 /*
-	Best viable option:
-		both meow function are viable but only the best viable are choosen.
-		least argument conversion is the best viable match.
-
-*/
-#include <ostream>
-#include <iostream>
-#include <string>
-using namespace std;
-
-template <int N>void meow(const char (&)[N]){ 
-  cout << "void meow(const char (&)[N])" << endl;
-}
-void meow(const string & ){
-  cout << "meow(const string & )" <<endl;
-}
-int main(){
-  meow("abc"); // which function to choose ?? (Best viable option)
-  string st("bob"); // which function to choose?? (Best Viable option)
-	meow(st);
-}
-
-#endif
-
-
-#if TEST19
-/*
-	Best viable option:
-		both meow function are viable but only the best viable are choosen.
-		least argument conversion is the best viable match.
-
-*/
-#include <ostream>
-#include <iostream>
-#include <string>
-using namespace std;
-
-template <int N>void meow(const char (&)[N]){ 
-  cout << "void meow(const char (&)[N])" << endl;
-}
-void meow(const string & ){
-  cout << "meow(const string & )" <<endl;
-}
-int main(){
-  meow("abc"); // which function to choose ?? (Best viable option)
-  string st("bob"); // which function to choose?? (Best Viable option)
-	meow(st);
-}
-
-#endif
-
-
-#if TEST20
-/*
-	Trickly question:
-		what will be output of the follwoing program?
-	Both the function are the viable candidate.
-		but which one is the most viable candidate?
-		int * ==>  const int* [ qualification conversion ]
-		int* ==> const (int*) & [ identity conversion ]
-
-	moral of story .. Template are greedy :)
-
-
-*/
-#include <ostream>
-#include <iostream>
-#include <string>
-using namespace std;
-
-/* TAD will deduce the T as int*  so the argument become "const (int*) &m" ===> m is a reference to "integer pointer which is constant" */
-template <typename T>void foo(const T& m){  // m is a reference to type T which is constant. === m is a referece to constant T
-
-  cout << "template <typename T>void foo(const T&)" << endl;
-}
-/* TAD will decude T as "int" so the argument become "const (int)* k" ==>  k is a pointer to an constant integer  */
-template <typename T>void foo(const T* k){ 
-  cout << "template <typename T>void foo(const T *)" << endl;
-}
-
-int main(){
-  int *p = nullptr;
-  foo(p);
-}
-
-#endif
-
-
-#if TEST21
-/*
-	Trickly question:
-	(tie breaker rule)
-		minimum distance conversion. 
-		Morederived* is convered to Derived *
-
-*/
-#include <ostream>
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-class Base {};
-class Derived :public Base {};
-class MoreDerived : public Derived {};
-
-void foo(Base *){
-  cout << " i am inside void foo(Base *)" <<endl;
-}
-
-void foo(Derived *){
-  cout << " i am inside void foo(Derived *)" <<endl;
-}
-
-int main(){
-  MoreDerived *md = new MoreDerived();
-  foo(md);
-
-}
-
-#endif
-
-
-#if TEST22
-/*
-		shared_ptr is part of standand library not the core C++ language.
-		All UDC (user defined conversion) are considered indistinquishable. (there is no way to prefer one over other)  
-		so an ambiguity arises here.
-
-		to fix the static_cast to fix this.
-
 */
 #include <ostream>
 #include <iostream>
@@ -192,23 +59,184 @@ int main(){
 using namespace std;
 
 
-class Base {};
-class Derived :public Base {};
+class Base {
+public: // you need meow to be public before using it as p->meow(); in dervied class meow can be private virtual.
+  void virtual  meow(){ // "virtual is must" to indicate that take the dyanmic type of the object in consideration beforing selecting the imlementation
+	cout <<"Base" <<endl;
+  }
+};
+class Derived :public Base {
+private : // being private Does not have effect on  what meow() is choosen !! REMEMBER VIRTUAL FUNCTION MECHANISM IS RUNTIME AND COMPLETE INDEPENDENT WITH THE COMPILE TIME ACCESS SPECIFIER
+  void virtual meow(){
+	cout <<"Derived" <<endl;
+  
+  }
+};
 class MoreDerived : public Derived {};
 
-void foo(shared_ptr<Base> &){
-  cout << " i am inside void foo(shared_ptr<Base> *)" <<endl;
-}
 
-void foo(shared_ptr<Derived> &){
-  cout << " i am inside void foo(shared_ptr<Derived> *)" <<endl;
-}
 
 int main(){
-  shared_ptr<MoreDerived> spmd;
-  //shared_ptr<Derived> spd = static_cast<shared_ptr<Derived>>(spmd); // to make it work do a static cast 
-  //foo(spd);
-  foo(smpd);
+  Base *p = new MoreDerived();
+  // if meow() is virtual the implementation is choosen based on dynamic type of p.
+  // if meow is non virtual the implementation to execute is choosen based on static type of *p
+  p->meow(); // static type of *p is Base (what compiler sees). dynamic type of *p is derived.
+
+}
+
+#endif
+/* 
+	understanding slicing and how to avoid that. ==> make the copy constructor private;
+	understanding the memory leak which can be caused by non virtual Destructors ; make destructor virtual
+
+*/
+
+#if TEST24
+/*
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+class Base {
+  int b;
+public:
+  virtual ~Base(){ cout<< "base Destructor" <<endl;}; // virtual destructors is needed. correct destructor get called. when you delete basep pointing to derived; otherwise you will leak derived part in the class
+  Base(){}
+private : 
+  Base (const Base &){} // make the copy constructor private to avoid the slicing. compiler will not generate the default copy constructor in this case
+};
+// derived become copyable !!!.
+class Derived :public Base {
+  int d;
+public:
+  Derived(){}
+  ~Derived(){  cout<< "Derived Destructor" <<endl;}
+};
+
+
+int main(){
+  Base *p = new Derived();
+  //  Base b2 = *p; // the implicit generated copy constructor will slice the object. Will  Only copy over the base class memeber to b2.So make the copy constructor private to error out at compile time
+  delete p;
+}
+
+#endif
+/* 
+   	demonstrate use of shared pointer to avoid memory leaks
+	demonstrate the masking of virtual function when the parameters are different.
+		you should always use same signature for the virtual functions in the hierarchy down the chain to avoid the masking.
+
+	C++ 11 have two context sensitive keyword. override and final.
+    use override keyword for compiler to erroring out indicating the programmer when the masking is happening. [ C++ 11 feature]
+    use final to avoid derivation of class. lock down the class 
+	
+*/
+
+#if TEST25
+/*
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+class Shape {
+  int b;
+public:
+  virtual void draw(int x,int y){
+	cout << "inside Shape::draw" << "x:" << x << "y:" << y <<endl;
+  }
+  virtual ~Shape(){ cout<< "Shape Destructor" <<endl;}; // virtual destructors is needed. correct destructor get called. when you delete basep pointing to derived; otherwise you will leak derived part in the class
+  Shape(){}
+private :
+  Shape (const Shape &){} // make the copy constructor private to avoid the slicing. compiler will not generate the default copy constructor in this case
+};
+
+class Polygon :public Shape {
+  int d;
+public:
+  Polygon(){}
+  virtual void  draw(int x,int y) override { // c++ 11 feature. no effect here but if you put it in Evilgon draw it will give a compile error to indicate there is a problem in code.
+	cout << "inside Polygon::draw" << " x:" << x << " y: " << y <<endl;
+  }
+  ~Polygon(){  cout<< "Polygon  Destructor" <<endl;}
+};
+class Evilgon :public Shape {
+  int d;
+public:
+  Evilgon(){}
+  virtual void draw(double x,double  y)  { // NOTICE THE ARGUMENTS HERE.  // NO overriding happens as the argument are different 
+	cout << "inside Polygon::draw" << " x: " << x << " y: " << y <<endl;
+  }
+  ~Evilgon(){  cout<< "Evilygon  Destructor" <<endl;}
+};
+
+int main(){
+  shared_ptr<Shape> shape = make_shared<Polygon>(); // shared ptr to avoid memory leak
+  shape->draw(11,22);
+  shape = make_shared<Evilgon>();
+  shape->draw(33,22);
+
+}
+
+#endif
+
+/* 
+   	Demonstrate NVI (Non Virtual interface) Ideam. Kind of design pattern to structure your code such that user of the class
+    only uses non virtual interfaces of the base class but base class internally can use the virtual mechanism to do work in
+    it functions.
+
+	
+*/
+
+#if TEST26
+/*
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+class Shape {
+  int b;
+public:
+  void draw(int x,int y){ // NOTE: This is an non virtual function 
+	cout<< " NVI starts here" <<endl;
+	do_draw(x,y);
+	cout<< " NVI ends here" <<endl;
+  }
+  virtual ~Shape(){ cout<< "Shape Destructor" <<endl;}; // virtual destructors is needed. correct destructor get called. when you delete basep pointing to derived; otherwise you will leak derived part in the class
+  Shape(){}
+protected : //notice protected so that derived can access the fucntion.
+  virtual void do_draw(int x,int y){
+	cout << "inside Shape::draw" << "x:" << x << "y:" << y <<endl;
+  }
+private :
+  Shape (const Shape &){} // make the copy constructor private to avoid the slicing. compiler will not generate the default copy constructor in this case
+};
+
+class Polygon :public Shape {
+  int d;
+public:
+  Polygon(){}
+  ~Polygon(){  cout<< "Polygon  Destructor" <<endl;}
+private:
+  virtual void  do_draw(int x,int y){ // c++ 11 feature. no effect here but if you put it in Evilgon draw it will give a compile error to indicate there is a problem in code.
+    Shape::do_draw(x,y); //QUALIFICATION OF FUCNTION NAME OVERRIDES THE VIRTUAL MECHANISM. do_draw() in base should be protected or public.
+	cout << "inside Polygon::draw" << " x:" << x << " y: " << y <<endl;
+  }
+
+};
+
+
+int main(){
+  shared_ptr<Shape> shape = make_shared<Polygon>(); // shared ptr to avoid memory leak
+  shape->draw(11,22);
 }
 
 #endif
