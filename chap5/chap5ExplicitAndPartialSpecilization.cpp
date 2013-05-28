b74573a40bf8a76d395d8f3b3cbe30f32e54a05b
DeepakPant <dpant@gmail.com>
2013-05-27 18:27:22 -0700

    final version of template specilization

--- chap5/chap5ExplicitAndPartialSpecilization.cpp
+++ chap5/chap5ExplicitAndPartialSpecilization.cpp
@@ -17,7 +17,7 @@ http://channel9.msdn.com/Series/C9-Lectures-Stephan-T-Lavavej-Core-C-/Stephan-T-
 */
 #include <iostream>
 #include <ostream>
-#define TEST26 1 
+#define TEST32 1 
 
 
 /*
@@ -36,14 +36,16 @@ http://channel9.msdn.com/Series/C9-Lectures-Stephan-T-Lavavej-Core-C-/Stephan-T-
 using namespace std;
 
 /* 
-   	Demonstrate NVI (Non Virtual interface) Ideam. Kind of design pattern to structure your code such that user of the class
-    only uses non virtual interfaces of the base class but base class internally can use the virtual mechanism to do work in
-    it functions.
-
+	explicity specilization of class template  (full specilization)
+		 
+	match happens in the following order
+		 Explicity specilization
+		 partial specilization
+		 prmary template
 	
 */
 
-#if TEST26
+#if TEST27
 /*
 */
 #include <ostream>
@@ -52,41 +54,207 @@ using namespace std;
 #include <memory> // for shared_ptr
 using namespace std;
 
-class Shape {
-  int b;
+
+template <typename T> class Kitty{ // primary template 
 public:
-  void draw(int x,int y){ // NOTE: This is an non virtual function 
-	cout<< " NVI starts here" <<endl;
-	do_draw(x,y);
-	cout<< " NVI ends here" <<endl;
+  static void meow(){
+	cout<<" kitty::Meow()!" <<endl;
   }
-  virtual ~Shape(){ cout<< "Shape Destructor" <<endl;}; // virtual destructors is needed. correct destructor get called. when you delete basep pointing to derived; otherwise you will leak derived part in the class
-  Shape(){}
-protected : //notice protected so that derived can access the fucntion.
-  virtual void do_draw(int x,int y){
-	cout << "inside Shape::draw" << "x:" << x << "y:" << y <<endl;
-  }
-private :
-  Shape (const Shape &){} // make the copy constructor private to avoid the slicing. compiler will not generate the default copy constructor in this case
 };
 
-class Polygon :public Shape {
-  int d;
+template <> class Kitty<double *>{ // template explicit specilazation. have to be exact match.
 public:
-  Polygon(){}
-  ~Polygon(){  cout<< "Polygon  Destructor" <<endl;}
-private:
-  virtual void  do_draw(int x,int y){ // c++ 11 feature. no effect here but if you put it in Evilgon draw it will give a compile error to indicate there is a problem in code.
-    Shape::do_draw(x,y); //QUALIFICATION OF FUCNTION NAME OVERRIDES THE VIRTUAL MECHANISM. do_draw() in base should be protected or public.
-	cout << "inside Polygon::draw" << " x:" << x << " y: " << y <<endl;
+  static void meow(){
+	cout<<" template <> class Kitty<double *>" <<endl;
   }
+};
 
+template <typename X> class Kitty<X *>{ // template partial specilazation. will match any pointer
+public:
+  static void meow(){
+	cout<< "<X> class Kitty<X *>" <<endl;
+  }
 };
 
 
 int main(){
-  shared_ptr<Shape> shape = make_shared<Polygon>(); // shared ptr to avoid memory leak
-  shape->draw(11,22);
+  Kitty<int>::meow();
+  Kitty<double *>::meow();
+  Kitty<const double *>::meow();
+}
+
+#endif
+
+/* 
+	explicity specilization of class template  (full specilization)
+		 
+	match happens in the following order
+		 Explicity specilization
+		 partial specilization
+		 prmary template
+	
+*/
+
+#if TEST28
+/*
+	shows partial specilization can lead to ambiguity in some cases which need to be resolved having another parital specilized template
+  
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+
+template <typename T,typename U> class Kitty{ };// primary template 
+template <typename A,typename B> class Kitty<A*,B>{ };// partial specialization 
+template <typename C,typename D> class Kitty<C,D*>{ };// partial specialization 
+template <typename E,typename F> class Kitty<E*,F*>{ };// partial specialization  -- this is needed otherwise compile time error (Ambiguity) !!!!
+
+int main(){
+  Kitty<int*,int*> k ;
+}
+
+#endif
+
+
+#if TEST29
+/*
+	explicit specializing of function template.
+	There is no partial specilization possible of function template.!
+
+     check how function specilization is different from function overloading.
+
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+template <typename T> void meow(T){
+  cout << "meow(T)"<<endl;
+}
+template <>void meow(long){
+  cout << "meow(long ) with explicity specilization"<<endl;
+}
+void meow(long x){ //overloading the function. this is not the specilization of template.!
+  cout << "meow(long) this is overloading of function"<<endl;
+}
+int main(){
+  meow(111);
+  meow(0L);
+}
+
+#endif
+
+
+#if TEST30
+/*
+	explicit specializing of function template.
+	There is no partial specilization possible of function template.!
+
+     check how function specilization is different from function overloading.
+
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+template <typename T> void meow(T){
+  cout << "meow(T)"<<endl;
+}
+template <typename X>void meow(X* ){ // overload of a function above, not a partial specialization.
+  cout << "meow(X* ) is an overload"<<endl;
+}
+
+int main(){
+  meow(111);
+  meow(0L);
+  meow(static_cast<double *>(nullptr));
+}
+
+#endif
+
+
+#if TEST31
+/*
+	explicit specializing of function template.
+	There is no partial specilization possible of function template.!
+	This one error outs!! trying to do a parital specialization
+
+    Notice how function specilization is different from function overloading.
+
+    error: function template partial specialization 'meow<X*>' is not allowed
+
+	use type_tratis and helpers if you want to do something like this.
+    or you can use classes as a wrapper to same function name and do the specilizatoin 
+    in those classes
+
+
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+template <typename T> void meow(){
+  cout << "meow(T)"<<endl;
+}
+template <typename X>void meow<X*>(){ // overload of a function above, not a partial specialization.
+  cout << "meow(X* ) is an overload"<<endl;
+}
+
+int main(){
+  meow<int>();
+  meow<long>();
+  meow<double *>();
+}
+
+#endif
+
+
+#if TEST32
+/*
+	explicit specializing of function template.
+	There is no partial specilization possible of function template.!
+
+    You can use classes as a wrapper to same function name and do the specilizatoin 
+    in those classes
+
+	Explicit instantiation of templates  ==> "templalte class meow<int>"; will spit out 
+   code for meow<int> template even if it is not used.
+
+	
+*/
+#include <ostream>
+#include <iostream>
+#include <string>
+#include <memory> // for shared_ptr
+using namespace std;
+
+template <typename T> class wrapper { // primary template 
+public:
+static void meow(){
+  cout << "meow(T)"<<endl;
+}
+};
+template <typename Z> class wrapper<Z*>{ //partial specilization
+public:
+  static void meow(){
+	cout << "meow(T) specilization for pointers"<<endl;
+  }
+  
+
+};
+
+int main(){
+  wrapper<int>::meow();
+  wrapper<double *>::meow();
 }
 
 #endif
